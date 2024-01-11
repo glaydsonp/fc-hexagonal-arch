266552b1e3a6f504a79d326cd6b4cfe6c44b36fe
Glaydson Prado <glaydson.prado@performait.com>
2024-01-11 11:47:30 -0300

    feat: implement function isValid in product

--- application/product.go
+++ application/product.go
@@ -2,7 +2,12 @@ package application
 
 import (
 	"errors"
+	"github.com/asaskevich/govalidator"
 )
+
+func init () {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type ProductInterface interface {
 	IsValid() (bool, error)
@@ -20,13 +25,31 @@ const (
 )
 
 type Product struct {
-	ID string
-	Name string
-	Status string
-	Price float64
+	ID string `valid:"uuidv4"`
+	Name string `valid:"required"`
+	Status string `valid:"required"`
+	Price float64 `valid:"float,optional"`
 }
 
 func (p *Product) IsValid() (bool, error) {
+	if p.Status == "" {
+		p.Status = DISABLED
+	}
+
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("The status must be enabled or disabled")
+	}
+
+	if p.Price < 0 {
+		return false, errors.New("The price must be greater than zero")
+	}
+
+	_, err := govalidator.ValidateStruct(p)
+
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
 
