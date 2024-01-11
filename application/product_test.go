b244e792ec6706ac385f67cf6a880da49f63b0c0
Glaydson Prado <glaydson.prado@performait.com>
2024-01-11 11:36:30 -0300

    test: disable product

--- application/product_test.go
+++ application/product_test.go
@@ -23,3 +23,22 @@ func TestProduct_Enable(t *testing.T) {
 	err = product.Enable()
 	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
 }
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Test"
+	product.Status = application.DISABLED
+	product.Price = 0
+	
+	err := product.Disable()
+	require.Nil(t, err)
+	
+	product.Price = -10
+	err = product.Disable()
+	require.Nil(t, err)
+	
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "The price must be less than or equal to zero in order to disable the product", err.Error())
+	
+}
