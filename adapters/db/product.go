5b6f8acc200a1ffd4aeca1cf3487998fa0cf4f94
Glaydson Prado <glaydson.prado@performait.com>
2024-01-12 09:46:26 -0300

    feat: add create/update on db adapter

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -34,6 +34,17 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 }
 
 func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+	p.db.QueryRow("select count(*) from products where id = ?", product.GetID()).Scan(&rows)
+
+	if rows == 0 {
+		return p.create(product)
+	}
+
+	return p.update(product)
+}
+
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
 	stmt, err := p.db.Prepare("insert into products(id, name, status, price) values(?, ?, ?, ?)")
 
 	if err != nil {
@@ -41,6 +52,28 @@ func (p *ProductDb) Save(product application.ProductInterface) (application.Prod
 	}
 
 	_, err = stmt.Exec(product.GetID(), product.GetName(), product.GetStatus(), product.GetPrice())
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare("update products set name = ?, status = ?, price = ? where id = ?")
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(product.GetName(), product.GetStatus(), product.GetPrice(), product.GetID())
 
 	if err != nil {
 		return nil, err
